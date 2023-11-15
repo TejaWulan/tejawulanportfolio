c100a63d07c554e220508c1d1764860379b68508
TejaWulan <69300622+TejaWulan@users.noreply.github.com>
2023-11-15 20:53:16 +0700

    CHANGES Resue and add projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ProjectBox from './ProjectBox';
 import NewsletterImage from '../images/wideeduwebsite1.png';
+import WideEduWebsite2 from '../images/wideeduwebsite2.png';
 import RogfreeImage from '../images/Java WCC.webp';
+import MySQL from '../images/My SQL WCC.webp';
 import TindogImage from '../images/wiress-logo.png';
+
 
 const Projects = () => {
   return (
@@ -10,8 +13,10 @@ const Projects = () => {
       <h1 className='projectHeading'>My <b>Projects</b></h1>
       <div className='project'>
         <ProjectBox projectPhoto={NewsletterImage} projectName="WIDE Edu Website" />
-        <ProjectBox projectPhoto={RogfreeImage} projectName="Java Basic Proramming" />
+        <ProjectBox projectPhoto={RogfreeImage} projectName="Java Basic Programming" />
+        <ProjectBox projectPhoto={WideEduWebsite2} projectName="WIDE Edu Website currently" />
         <ProjectBox projectPhoto={TindogImage} projectName="WIRESS Clean Code App" />
+        <ProjectBox projectPhoto={MySQL} projectName="MySQL Course" />
       </div>
 
     </div>
